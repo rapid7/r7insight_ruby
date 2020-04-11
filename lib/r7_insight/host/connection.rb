58c2ec4af1ebe63d10d3157632f7e00387be0336
Suhas Sharma <sharma@suhastech.com>
2020-04-11 20:05:29 +0530

    Correct the usage of defined? for min_version

--- lib/r7_insight/host/connection.rb
+++ lib/r7_insight/host/connection.rb
@@ -188,10 +188,10 @@ module R7Insight
             ssl_context = OpenSSL::SSL::SSLContext.new
             ssl_context.cert_store = cert_store
 
-            if defined?(OpenSSL::SSL::SSLContext.method_defined? :min_version)
+            if defined?(ssl_context.min_version)
               ssl_context.min_version = OpenSSL::SSL::TLS1_1_VERSION
             end
-            if defined?(OpenSSL::SSL::SSLContext.method_defined? :max_version)
+            if defined?(ssl_context.max_version)
               # For older versions of openssl (prior to 1.1.1) missing support for TLSv1.3
               ssl_context.max_version = if defined?(OpenSSL::SSL::TLS1_3_VERSION)
                                           OpenSSL::SSL::TLS1_3_VERSION
